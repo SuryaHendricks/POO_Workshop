bf7bf6cb40902ab4615a450a024db4f0ad58e96f
Surya <hendricks.surya@gmail.com>
2020-02-19 10:20:02 +0100

    final commit

--- POO_exercices/common/menu.php
+++ POO_exercices/common/menu.php
@@ -5,6 +5,5 @@
         <li><a href="exo1.php">Exo1</a></li>
         <li><a href="exo2.php">Exo2</a></li>
         <li><a href="exo3.php">Exo3</a></li>
-        <li><a href="exo4.php">Exo4</a></li>
     </ul>
 </nav>
